df50f1ce0265f5004961d1936a1b6b9a9e71091b
garru <garruswasab@gmail.com>
2021-07-02 16:10:39 +0200

    Add project files.

---
+++ Tresor001/Controllers/ReviewController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos.Table;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Tresor001.Controllers
+{
+    [Route("api/Review")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private string storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=tresor01;AccountKey=upMX4oCjopXt8t+l2xPoHRqS1zoIITBebAtUXfeGlGTqSV47KKVNZ/XQ7pLTjqWddz1PSScDQcw5ICyO9JMdHA==;EndpointSuffix=core.windows.net";
+        private string tableName = "tresor001";
+        public bool viewedLast = true;
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            CloudStorageAccount storageAccount;
+            storageAccount = CloudStorageAccount.Parse(storageConnectionString);
+
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient(new TableClientConfiguration());
+            CloudTable table = tableClient.GetTableReference(tableName);
+            var reviews = table.ExecuteQuery(new TableQuery<Review>()).ToList();
+            var sortedreviews = reviews.OrderBy(x => x.Timestamp);
+            JObject json = new JObject();
+            json["reviews"] = JToken.FromObject(sortedreviews);
+            viewedLast = true;
+            return Ok(json);
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            if (viewedLast == false)
+            {
+                return BadRequest("Can't write a review without viewing the previous ones.");
+            }
+            else
+            {
+                return Ok();
+            }
+        }
+    }
+}
---
+++ Tresor001/Review.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Azure.Cosmos.Table;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tresor001
+{
+    public class Review : TableEntity
+    {
+        public Review() { }
+
+        public Review(string partition, string row)
+        {
+            PartitionKey = partition;
+            RowKey = row;
+        }
+
+        public string text { get; set; }
+
+        public override string ToString()
+        {
+            return text;
+        }
+    }
+}
